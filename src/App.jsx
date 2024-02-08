3ba6ef1ca8b6a2d43c57777208aa94a32cad3640
Dk2626 <dineshkumardssd@gmail.com>
2024-02-08 16:49:04 +0530

    url added

--- src/App.jsx
+++ src/App.jsx
@@ -8,11 +8,12 @@ const App = () => {
   useEffect(() => {
     // Replace the URL with the actual API endpoint
     // const apiUrl = 'http://13.232.171.97:4000/api/blogs/getblogs';
-    const apiUrl = 'http://54.91.48.93:3001';
+    const apiUrl = 'http://54.163.191.159:3001';
 
     axios
       .get(apiUrl)
       .then((response) => {
+        console.log('response', response);
         setData(response.data);
       })
       .catch((error) => {
